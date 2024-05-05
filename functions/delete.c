10feef5ec7bde6999e387fb532cc1e211e4a052c
Carolina <cdarocha@web.de>
2024-05-05 20:05:09 +0200

    delete also working

--- functions/delete.c
+++ functions/delete.c
@@ -32,6 +32,7 @@ void mydelete(char* filename, long directory_address, FILE* in) {
     fseek(in, directory_address, SEEK_SET); // go to begin of directory
     do
     {
+        long curr_addr = ftell(in);
         fread(&entry, sizeof(entry), 1, in);
         if (entry.filename[0] != 0x00)
         {
@@ -41,25 +42,34 @@ void mydelete(char* filename, long directory_address, FILE* in) {
 
             if (strcmp(temp, name_padded) == 0)
             {
+                fseek(in, curr_addr, SEEK_SET);
+                char deleted_sign = 0xE5;
+                fwrite(&deleted_sign, 1, 1, in);
+
                 unsigned int cluster_size = bs.sectors_per_cluster * bs.sector_size;
                 long boot_sector_start = bs.sector_size * pt[0].start_sector;
                 long fat_start = boot_sector_start + cluster_size; // boot block has one sector
                 long entry_block_in_fat = entry.starting_cluster / 200;
                 long entry_in_fat = fat_start + (entry_block_in_fat * cluster_size) + (entry.starting_cluster * 2);
 
-                unsigned short current_cluster = 8;          // actual cluster
+                unsigned short current_cluster = entry.starting_cluster;          // actual cluster
                 unsigned short next_cluster = 0;              // next cluster
 
                 while (current_cluster != 0xFFFF)
                 {
                     long entry_in_fat = fat_start + (current_cluster / 200 * cluster_size) + (current_cluster * 2);
                     fseek(in, entry_in_fat, SEEK_SET);
+                    unsigned short zero = 0;
                     fread(&next_cluster, 2, 1, in);
-                    printf("current_cluster: %X\n", current_cluster);
-                    printf("next_cluster: %X\n", next_cluster);
+                    fseek(in, entry_in_fat, SEEK_SET);
+                    fwrite(&zero, sizeof(short), 1, in);
 
+                    //printf("entry_in_fat: %X\n", entry_in_fat);
+                    //printf("current_cluster: %X\n", current_cluster);
+                    printf("next_cluster: %X\n", next_cluster);
+                    
                     current_cluster = next_cluster;
-                }
+                } 
             }
         }
     } while (entry.filename[0] != 0x00);
